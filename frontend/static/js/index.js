a91e7abec82dae871b14c140d1f7091ba22b23c9
Build Service <buildservice@mvpprojects.com>
2023-06-24 16:42:39 +0200

    Update

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,4 +1,4 @@
-console.log("JS is loaded!")
+import Dashboard from "./views/Dashboard.js"
 
 const navigateTo= url =>
 {
@@ -8,9 +8,10 @@ const navigateTo= url =>
 
 const router = async () => {
     const routes = [
-        { path: "/", view: () => console.log("Viewing Dashboard") },
-        { path: "/posts", view: () => console.log("Viewing Posts") },
-        { path: "/settings", view: () => console.log("Viewing Settings") },
+        { path: "/", view: Dashboard },
+        // { path: "/", view: () => console.log("Viewing Dashboard") },
+        // { path: "/posts", view: () => console.log("Viewing Posts") },
+        // { path: "/settings", view: () => console.log("Viewing Settings") },
     ];
     // Test each route for potential match
     const potentialMatches = routes.map(route =>
@@ -31,6 +32,11 @@ const router = async () => {
             isMatch: true,
         };
     }
+
+    const view = new match.route.view()
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+
     console.log(match.route.view());
 };
 
