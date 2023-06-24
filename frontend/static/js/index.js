2607758c8df583c87306b30e007803b511b422ec
Build Service <buildservice@mvpprojects.com>
2023-06-24 15:30:14 +0200

    Update

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,2 +1,49 @@
 console.log("JS is loaded!")
 
+const navigateTo= url =>
+{
+    history.pushState(null, null, url);
+    router();
+}
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Viewing Dashboard") },
+        { path: "/posts", view: () => console.log("Viewing Posts") },
+        { path: "/settings", view: () => console.log("Viewing Settings") },
+    ];
+    // Test each route for potential match
+    const potentialMatches = routes.map(route =>
+    {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if (!match)
+    {
+        match =
+        {
+            route: routes[0],
+            isMatch: true,
+        };
+    }
+    console.log(match.route.view());
+};
+
+ 
+document.addEventListener("DOMContentLoaded", () =>
+{
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]"))
+        {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
