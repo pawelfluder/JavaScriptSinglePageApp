45f0e019e11917f861f0c5bcad9b5b9918a57679
Build Service <buildservice@mvpprojects.com>
2023-06-24 15:07:46 +0200

    Update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
+
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve("frontend", "index.html"))
 });
